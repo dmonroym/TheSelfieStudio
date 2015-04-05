4af662cfc883aa6f90819ec8e1d27ce5f235fb19
Daniel <dmonroy@utexas.edu>
2015-04-05 16:19:22 -0500

    Fixed problem with keyhash on facebook login closes #1

--- app/src/main/java/com/cs371m/theselfiestudio/LoginActivity.java
+++ app/src/main/java/com/cs371m/theselfiestudio/LoginActivity.java
@@ -2,8 +2,13 @@ package com.cs371m.theselfiestudio;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.content.pm.Signature;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
+import android.util.Base64;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -16,20 +21,24 @@ import com.facebook.login.LoginManager;
 import com.facebook.login.LoginResult;
 import com.facebook.login.widget.LoginButton;
 
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
 public class LoginActivity extends FragmentActivity {
     CallbackManager callbackManager;
     @Override
     public void onCreate(Bundle savedInstanceState) {
 
-    final Intent intent = new Intent(this, MainActivity.class);
-//        startActivity(intent);
-
-
         super.onCreate(savedInstanceState);
+
         FacebookSdk.sdkInitialize(this.getApplicationContext());
         setContentView(R.layout.activity_login);
 
         callbackManager = CallbackManager.Factory.create();
+
+        final Intent intent = new Intent(this, MainActivity.class);
+        //uncomment next line in order to skip the facebook login activity
+        // startActivity(intent);
 
         LoginManager.getInstance().registerCallback(callbackManager,
                 new FacebookCallback<LoginResult>() {
@@ -49,12 +58,5 @@ public class LoginActivity extends FragmentActivity {
                     }
                 });
     }
-
-
-
-
-
-
-
 
 }

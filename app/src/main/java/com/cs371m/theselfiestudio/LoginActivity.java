5f03ef0f80648b700d2d05d6eb10fab50976c0e5
Daniel <dmonroy@utexas.edu>
2015-04-05 23:31:26 -0500

    FB Log in button has expected behavior. When logged in you're sent to MainActivity right away

--- app/src/main/java/com/cs371m/theselfiestudio/LoginActivity.java
+++ app/src/main/java/com/cs371m/theselfiestudio/LoginActivity.java
@@ -1,62 +1,105 @@
 package com.cs371m.theselfiestudio;
 
-import android.app.Activity;
+import android.app.AlertDialog;
 import android.content.Intent;
-import android.content.pm.PackageInfo;
-import android.content.pm.PackageManager;
-import android.content.pm.Signature;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
-import android.util.Base64;
 import android.util.Log;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
 
+import com.facebook.AccessToken;
 import com.facebook.CallbackManager;
 import com.facebook.FacebookCallback;
 import com.facebook.FacebookException;
 import com.facebook.FacebookSdk;
+import com.facebook.appevents.AppEventsLogger;
 import com.facebook.login.LoginManager;
 import com.facebook.login.LoginResult;
-import com.facebook.login.widget.LoginButton;
-
-import java.security.MessageDigest;
-import java.security.NoSuchAlgorithmException;
 
 public class LoginActivity extends FragmentActivity {
-    CallbackManager callbackManager;
+
+    private CallbackManager callbackManager;
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        // Call the 'activateApp' method to log an app event for use in analytics and advertising
+        // reporting.  Do so in the onResume methods of the primary Activities that an app may be
+        // launched into.
+        AppEventsLogger.activateApp(this);
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        callbackManager.onActivityResult(requestCode, resultCode, data);
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+
+        // Call the 'deactivateApp' method to log an app event for use in analytics and advertising
+        // reporting.  Do so in the onPause methods of the primary Activities that an app may be
+        // launched into.
+        AppEventsLogger.deactivateApp(this);
+    }
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
+        Log.d("LoginActivity","onCreate");
 
         super.onCreate(savedInstanceState);
 
         FacebookSdk.sdkInitialize(this.getApplicationContext());
-        setContentView(R.layout.activity_login);
 
         callbackManager = CallbackManager.Factory.create();
 
-        final Intent intent = new Intent(this, MainActivity.class);
+        final Intent intent_main = new Intent(this, MainActivity.class);
+
         //uncomment next line in order to skip the facebook login activity
-        // startActivity(intent);
+        // startActivity(intent_main);
 
         LoginManager.getInstance().registerCallback(callbackManager,
                 new FacebookCallback<LoginResult>() {
                     @Override
                     public void onSuccess(LoginResult loginResult) {
-                        startActivity(intent);
+                        startActivity(intent_main);
+                        finish();
                     }
 
                     @Override
                     public void onCancel() {
-                        startActivity(intent);
+                            showAlert();
                     }
 
                     @Override
                     public void onError(FacebookException exception) {
-                        startActivity(intent);
+                            showAlert();
+                    }
+
+                    private void showAlert() {
+                        new AlertDialog.Builder(LoginActivity.this)
+                                .setTitle("CANCELLED")
+                                .setMessage("Permission not granted")
+                                .setPositiveButton("OK", null)
+                                .show();
                     }
                 });
-    }
 
+        setContentView(R.layout.activity_login);
+
+        //determine if the user has already logged in
+        boolean loggedIn = AccessToken.getCurrentAccessToken() != null;
+        if(loggedIn)
+        {
+            startActivity(intent_main);
+            //finish();
+        }
+    }
 }
